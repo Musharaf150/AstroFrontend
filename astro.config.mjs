2c9cc2584c2fadba86d40a33502ffad781601537
Muhammad Musharaf <mushi.ashraf38@gmail.com>
2025-01-17 17:19:35 +0500

    updated and fix pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,16 @@ import vercelAdapter from '@astrojs/vercel';
 import { defineConfig } from 'astro/config';
 
 
+import react from '@astrojs/react';
+
+
 // https://astro.build/config
 export default defineConfig({
   i18n: {
       defaultLocale: 'en',
       locales: ['ar', 'en'],
   },
+
   adapter: vercelAdapter(),
+  integrations: [react()],
 });
